0fb17523591d944652ee4a96ab5e1a88466c1595
Timur Karshiev <lollipop.fly@gmail.com>
2015-09-28 10:49:45 +0300

    add @media screen to sass file and function of detect window screen

--- js/common.js
+++ js/common.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-	// some text
+
+/**
+***************************************************************
+* =PAGE LOAD
+***************************************************************
+**/
+
+	// Находим ширину экрана
+	var screenWidth = $(document).width() + scrollWidth()
+
+
+/**
+***************************************************************
+* =FUNCTIONS
+***************************************************************
+**/
+
+// Функция определения плосы прокрутки
+	    function scrollWidth() {
+	    var div = $('<div>').css({
+	        position: "absolute",
+	        top: "0px",
+	        left: "0px",
+	        width: "100px",
+	        height: "100px",
+	        visibility: "hidden",
+	        overflow: "scroll"
+	    });
+
+	    $('body').eq(0).append(div);
+
+	    var width = div.get(0).offsetWidth - div.get(0).clientWidth;
+
+	    div.remove();
+
+	    return width;
+	}
 });
