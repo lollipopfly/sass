14cd4b78426fb2bd8315c3e2d458f424a203b39f
lollipopfly <lollipop.fly@gmail.com>
2016-04-26 18:57:48 +0300

    add mixins and extends

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,46 @@
 var gulp          = require('gulp'),
-	postcss       = require('gulp-postcss'),
-	browserSync   = require('browser-sync').create(),
-	sass          = require('gulp-sass'),
-	size          = require('postcss-size'),
-	pxtorem       = require('postcss-pxtorem'),
-	colorFunction = require("postcss-color-function"),
-	autoprefixer  = require('autoprefixer'),
-	concat        = require('gulp-concat'),
-	uglify        = require('gulp-uglify'),
-	selectors     = require('postcss-custom-selectors'),
-	spritesmith   = require('gulp.spritesmith'),
-	plumber       = require('gulp-plumber'),
-	notify        = require("gulp-notify"),
-	merge         = require('merge-stream');
+    postcss       = require('gulp-postcss'),
+    browserSync   = require('browser-sync').create(),
+    sass          = require('gulp-sass'),
+    size          = require('postcss-size'),
+    pxtorem       = require('postcss-pxtorem'),
+    colorFunction = require("postcss-color-function"),
+    postcssExtend = require('postcss-sass-extend'),
+    autoprefixer  = require('autoprefixer'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    selectors     = require('postcss-custom-selectors'),
+    spritesmith   = require('gulp.spritesmith'),
+    plumber       = require('gulp-plumber'),
+    notify        = require("gulp-notify"),
+    merge         = require('merge-stream');
 
 /*------------------------------------*\
 	Sass
 \*------------------------------------*/
 
 gulp.task('sass', function() {
-	var processors = [
-	autoprefixer({ browsers: ['last 20 versions'] }),
-		selectors,
-		size,
-		colorFunction,
-		pxtorem({
-			replace: true
-		})
+    var processors = [
+    autoprefixer({ browsers: ['last 20 versions'] }),
+        selectors,
+        postcssExtend,
+        size,
+        colorFunction,
+        pxtorem({
+            replace: true
+        })
 ];
 
   return gulp.src([
-		  'src/styles/_sprite.scss',
-		  // 'node_modules/slick-carousel/slick/slick.css',
-		  // 'node_modules/slick-carousel/slick/slick-theme.css',
-		  'src/styles/app.scss',])
-	.pipe(concat('style.css'))
-	.pipe(sass().on('error', error))
-	.pipe(sass({outputStyle: 'compressed'}))
-	.pipe(postcss(processors))
-	.pipe(gulp.dest('dist/css/'));
+        'src/styles/_sprite.scss',
+        // 'node_modules/slick-carousel/slick/slick.css',
+        // 'node_modules/slick-carousel/slick/slick-theme.css',
+        'src/styles/app.scss',])
+    .pipe(concat('style.css'))
+    .pipe(sass().on('error', error))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('dist/css/'));
 });
 
 
@@ -47,29 +49,29 @@ gulp.task('sass', function() {
 \*------------------------------------*/
 
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('public/images/main/*.png').pipe(spritesmith({
-	imgName: 'sprite.png',
-	cssName: '_sprite.scss',
-	imgPath: '../../public/images/sprite.png',
-	padding: 1,
-	'cssFormat': 'scss',
-	cssOpts: {
-	// for remove prefix icon-
-	cssSelector: function (sprite) {
-	  return '.' + sprite.name;
-	},
-	algorithmOpts: {sort: true}
-  }
+    var spriteData = gulp.src('public/images/main/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: '_sprite.scss',
+    imgPath: '../../public/images/sprite.png',
+    padding: 1,
+    'cssFormat': 'scss',
+    cssOpts: {
+    // for remove prefix icon-
+    cssSelector: function (sprite) {
+        return '.' + sprite.name;
+    },
+    algorithmOpts: {sort: true}
+    }
 }));
 
-  var imgStream = spriteData.img
-	.pipe(gulp.dest('public/images/'));
+    var imgStream = spriteData.img
+    .pipe(gulp.dest('public/images/'));
 
-  var cssStream = spriteData.css
-	.pipe(gulp.dest('src/styles/'));
+    var cssStream = spriteData.css
+    .pipe(gulp.dest('src/styles/'));
 
-  return merge(imgStream, cssStream);
-  // return spriteData.pipe(gulp.dest('css/'));
+    return merge(imgStream, cssStream);
+    // return spriteData.pipe(gulp.dest('css/'));
 });
 
 
@@ -78,14 +80,14 @@ gulp.task('sprite', function () {
 \*------------------------------------*/
 
 gulp.task('compress', function() {
-	return gulp.src([
-			'src/js/jquery.js',
-			// 'node_modules/slick-carousel/slick/slick.min.js',
-			'src/js/common.js'])
-		.pipe(plumber())
-		.pipe(concat('global.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js/'));
+    return gulp.src([
+            'src/js/jquery.js',
+            // 'node_modules/slick-carousel/slick/slick.min.js',
+            'src/js/common.js'])
+        .pipe(plumber())
+        .pipe(concat('global.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
 });
 
 
@@ -94,14 +96,14 @@ gulp.task('compress', function() {
 \*------------------------------------*/
 
 gulp.task('serve', ['sass'], function() {
-	browserSync.init({
-	server: "",
-	'port': 3000
-	});
+    browserSync.init({
+        server: "",
+        port: 3000,
+    });
 
-	gulp.watch("dist/js/**/*.js").on('change', browserSync.reload);
-	gulp.watch("dist/css/style.css").on('change', browserSync.reload);
-	gulp.watch("index.html").on('change', browserSync.reload);
+    gulp.watch("dist/js/**/*.js").on('change', browserSync.reload);
+    gulp.watch("dist/css/style.css").on('change', browserSync.reload);
+    gulp.watch("index.html").on('change', browserSync.reload);
 });
 
 /*------------------------------------*\
@@ -111,7 +113,7 @@ gulp.task('serve', ['sass'], function() {
 gulp.task('watch', function() {
 	gulp.watch('src/styles/**/*.scss', { interval: 500 }, ['sass', 'notify']);
 	gulp.watch('src/js/common.js', { interval: 500 }, ['compress', 'notify']);
-  // gulp.watch('public/images/main/*.png', { interval: 500 }, ['sprite']);
+    // gulp.watch('public/images/main/*.png', { interval: 500 }, ['sprite']);
 });
 
 /*------------------------------------*\
@@ -119,9 +121,9 @@ gulp.task('watch', function() {
 \*------------------------------------*/
 
 gulp.task('notify', function(a) {
-  var date = new Date();
-  gulp.src("public/css/style.css")
-  .pipe(notify("Css was compiled! at " + date));
+    var date = new Date();
+    gulp.src("public/css/style.css")
+    .pipe(notify("Css was compiled! at " + date));
 });
 
 /*------------------------------------*\
@@ -142,4 +144,3 @@ function error(error) {
   console.log(error.toString());
   this.emit('end');
 }
-
